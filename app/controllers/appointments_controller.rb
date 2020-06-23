4e0d1961c816faf137f58d46ed5ea2764bc5497f
Tammy Lam <lamtammy1515@gmail.com>
2020-06-23 23:01:47 +0000

    Automatically backed up by Learn

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,5 +1,5 @@
-class AppointmentsController < ApplicationController 
-  def show 
+class AppointmentsController < ApplicationController
+  def show
     @appointment = Appointment.find(params[:id])
-  end 
-end 
+  end
+end
